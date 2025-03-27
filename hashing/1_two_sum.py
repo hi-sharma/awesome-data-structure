4ad638f57d6c320db3808c0f360ad8198df871b6
Himanshu Sharma <57651260+hi-sharma@users.noreply.github.com>
2025-03-27 23:15:14 +0800

    Update 1_two_sum.py

--- hashing/1_two_sum.py
+++ hashing/1_two_sum.py
@@ -1,5 +1,7 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        # O(n) Time complexity
+        # O(n) Space complexity
         dic = {}
         for i in range(len(nums)):
             num = nums[i]
