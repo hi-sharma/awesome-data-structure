5d54635a038cc92e45caa945cfa398613af9e849
Himanshu Sharma <57651260+hi-sharma@users.noreply.github.com>
2025-03-27 23:18:06 +0800

    Update 2351_first_letter_to_appear_twice.py

--- hashing/2351_first_letter_to_appear_twice.py
+++ hashing/2351_first_letter_to_appear_twice.py
@@ -1,7 +1,7 @@
 class Solution:
     def repeatedCharacter(self, s: str) -> str:
         # O(n) time complexity
-        # O(n) space complexity
+        # O(n) space complexity; Space complexity could be O(1) if we only consider alphabets from English Language
         hashMap = set()
         for char in s:
             if char in hashMap:
