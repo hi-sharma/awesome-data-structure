b6b30d1e75fb9de8e5b03a25daa55727afb83bcf
Himanshu Sharma <57651260+hi-sharma@users.noreply.github.com>
2025-03-29 12:13:01 +0800

    Create lists.py

---
+++ python_tricks/lists.py
@@ -0,0 +1,15 @@
+
+def print_row_2Dmatrix(mat: List[List[*]]) -> List[]:
+  for row in mat:
+    print(row)
+
+def print_col_2Dmatrix(mat: List[List[*]]) -> List[]:
+  # get index in horizontal right direction for cols by iterating over first row
+  for col in range(len(mat[0])):
+    current_col = []
+    # get index in vertical down direction for rows 
+    for row in range(len(mat):
+      current_col.append(mat[row][col])
+    print(col)
+
+
