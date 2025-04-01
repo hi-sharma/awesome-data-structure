145d8870877c930f61fc817db446e5800abae560
Himanshu Sharma <57651260+hi-sharma@users.noreply.github.com>
2025-04-01 16:42:49 +0800

    Create [R]322_coin_change.py

---
+++ dynamic_programming/[R]322_coin_change.py
@@ -0,0 +1,49 @@
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        # DP with bottom up
+        # dp array[i] which takes i/states is the amount and return the min # of coins that you need to make that amount
+        # Space complexity O(amount) and time complexity O(amount*len(coins))
+        n = len(coins)
+        dp = [0]*(amount+1)
+        # base case
+        dp[0] = 0
+        # recurrence relation
+        for i in range(1, amount + 1):
+            min_val = float("inf")
+            for coin in coins:
+                if i - coin >= 0:
+                    min_val = min(min_val, 1 + dp[i - coin])
+            dp[i] = min_val
+        ans = dp[amount]
+        print(dp)
+        if ans == float("inf"):
+            return -1
+        return ans
+
+
+# # from functools import cache
+# class Solution:
+#     def coinChange(self, coins: List[int], amount: int) -> int:
+#         # DP with top down/ recursion + memo to save on repeated computations
+#         # dp func(i) which takes i/states is the amount and return the min # of coins that you need to make that amount
+#         # use memoisation to save states from 0 to amount. 
+#         # Space complexity O(amount) and time complexity O(amount*len(coins))
+#         # @cache
+#         def dp(i):
+#             # base case
+#             if i < 0:
+#                 return float("inf")
+#             if i == 0:
+#                 return 0
+#             if i in memo:
+#                 return memo[i]
+#             # recurrence relation            
+#             memo[i] = min([1 + dp(i-coin) for coin in coins])
+#             return memo[i]
+
+#         # call the dp and return
+#         memo = {}
+#         ans = dp(amount)
+#         if ans == float("inf"):
+#             return -1
+#         return ans
