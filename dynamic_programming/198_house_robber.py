2d0f2b82673b73d526e30e566669cdccabb6ea79
Himanshu Sharma <57651260+hi-sharma@users.noreply.github.com>
2025-04-01 14:36:41 +0800

    Update 198_house_robber.py

--- dynamic_programming/198_house_robber.py
+++ dynamic_programming/198_house_robber.py
@@ -1,5 +1,22 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
+        # Use DP with bottom up approach: O(n) time and space complexity
+        n = len(nums)
+        if n==1:
+            return nums[0]
+        robFrom = [0]*(n+1)
+        # base case
+        robFrom[n] = nums[n-1]
+        robFrom[n-1] = max(nums[n-1], nums[n-2])
+        # dp[i] = max(dp[i] + robFrom(dp[i+2], db[i+1])
+        for i in range(n-2, -1, -1):
+            robFrom[i] = max(nums[i] + robFrom[i+2], robFrom[i+1])
+        return robFrom[0]
+    
+
+
+class Solution:
+    def rob(self, nums: List[int]) -> int:
         # Use DP with top down approach: O(n) time and space complexity
         def robFrom(nums, i):
             n = len(nums)
